ec5b2dab3062d1e89c7b187deb78a46de3be6879
Daniel White <dw8405275@gmail.com>
2022-05-20 22:32:35 +1200

    Upgrade index.tsx to be ready for React 18.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import './react-contextmenu.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+if (null!==container) {
+createRoot(container).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode> 
 );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
